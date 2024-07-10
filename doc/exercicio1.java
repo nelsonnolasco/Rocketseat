885db72d9d18d8bce4ca9229e7f18ca43eb59a9c
Nelson de Campos Nolasco <nelsonnolasco@gmail.com>
2024-07-10 19:40:35 -0300

    Primeiro exercício java em doc.

---
+++ doc/exercicio1.java
@@ -0,0 +1,38 @@
+public class ContaCorrente {
+	int numeroConta;
+	int numeroAgencia;
+	String nomeCliente;
+	LocalDate dataNascimento;
+	double saldoConta;
+	boolean ativo = true;
+
+public void sacarValor(Double valorSolicitado) {
+
+	double valorSolicitado;
+
+}
+
+public void transferirValor(contaCorrente contaDestino, Double valorTransferido) {
+
+	double valorTransferido;
+
+}
+
+public void cancelarContaJustificativa(String justificativa) {
+
+	// algoritmo irá manipular o atributo
+	// ativo pasra que se torne false
+
+}
+
+list consultarExtrato(LocalDate dataInicial, LocalDate dataFinal) {
+
+	return ???;
+
+}
+
+Double consultarSaldoAtual() {
+
+	return saldo;
+
+}
